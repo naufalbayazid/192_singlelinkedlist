847325462834cdbe50174e73e70f8a931e53052e
naufalbayazid <naufalichwanbayazid@gmail.com>
2023-05-19 11:02:57 +0700

    masukkan data mhs

--- singlelinkedlist_192/singlelinkedlist_192.cpp
+++ singlelinkedlist_192/singlelinkedlist_192.cpp
@@ -9,3 +9,14 @@ struct Node
 };
 
 Node* START = NULL; 
+
+void addNote() {
+	int nim;
+	string nama;
+	Node* nodeBaru = new Node();
+	cout << "Masukkan NIM: ";
+	cin >> nim;
+	cout << "Masukkan Nama: ";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
